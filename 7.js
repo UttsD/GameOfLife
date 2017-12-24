9531f47c68db54b2e635ee071504151efbe39c39
Dariya Utts <Utts_d@outlook.com>
2017-12-24 23:07:41 +0300

    the worst realization ever(

--- 7.js
+++ 7.js
@@ -1,29 +1,33 @@
 "use strict";
 function make2DArray(cols, rows) {
     var arr = new Array(cols);
-    for (var i = 0; i < arr.length; i++) {
-        arr[i] = new Array(rows);
+    for (var i_1 = 0; i_1 < arr.length; i_1++) {
+        arr[i_1] = new Array(rows);
     }
     return arr;
 }
 var grid;
+var position;
 var cols;
 var rows;
 var resolutions = 10;
 var canvas;
 var ctx;
+var _canvasWidth = document.getElementById('canvasWidth').value;
+var _canvasHeight = document.getElementById('canvasHeight').value;
 var timer;
 function setup() {
     canvas = document.getElementById('cnvs');
     ctx = canvas.getContext("2d");
-    canvas.width = 800;
-    canvas.height = 600;
+    position = 0;
+    canvas.width = _canvasWidth;
+    canvas.height = _canvasHeight;
     cols = canvas.width / resolutions;
     rows = canvas.height / resolutions;
     grid = make2DArray(cols, rows);
-    for (var i = 0; i < cols; i++) {
+    for (var i_2 = 0; i_2 < cols; i_2++) {
         for (var j = 0; j < rows; j++) {
-            grid[i][j] = Math.floor(Math.random() * 2) + 0;
+            grid[i_2][j] = Math.floor(Math.random() * 2) + 0;
         }
     }
 }
@@ -43,6 +47,9 @@ function draw() {
         }
     }
 }
+
+
+
 function game() {
     var next = make2DArray(cols, rows);
     for (var i = 0; i < cols; i++) {
@@ -63,13 +70,12 @@ function game() {
     }
     grid = next;
     draw();
-    
 }
 function countNeighbors(grid, x, y) {
     var sum = 0;
-    for (var i = -1; i < 2; i++) {
+    for (var i_3 = -1; i_3 < 2; i_3++) {
         for (var j = -1; j < 2; j++) {
-            var col = (x + i + cols) % cols;
+            var col = (x + i_3 + cols) % cols;
             var row = (y + j + rows) % rows;
             sum += grid[col][row];
         }
@@ -77,14 +83,69 @@ function countNeighbors(grid, x, y) {
     sum -= grid[x][y];
     return sum;
 }
+for (var i = 0; i < 2; i++) {
+    document.getElementsByTagName('input')[i].onkeypress =
+        function NotANumber(e) {
+            e = e || event;
+            if (e.ctrlKey || e.altKey || e.metaKey)
+                return;
+            var chr = getChar(e);
+            if (chr == null)
+                return;
+            if (chr < '0' || chr > '9') {
+                return false;
+            }
+        };
+    function getChar(event) {
+        if (event.which == null) {
+            if (event.keyCode < 32)
+                return null;
+            return String.fromCharCode(event.keyCode);
+        }
+        if (event.which != 0 && event.charCode != 0) {
+            if (event.which < 32)
+                return null;
+            return String.fromCharCode(event.which);
+        }
+        return null;
+    }
+}
 window.onload = function () {
     setup();
     draw();
+    canvasWidth.onblur = function () {
+        _canvasWidth = this.value;
+        clearInterval(timer);
+        setup();
+        draw();
+    };
+    canvasHeight.onblur = function () {
+        _canvasHeight = this.value;
+        clearInterval(timer);
+        setup();
+        draw();
+    };
     start.onclick = function () {
         timer = setInterval("game()", 10);
     };
-
     pause.onclick = function () {
         clearInterval(timer);
+        canvas.addEventListener('click', function (event) {
+            var x = event.offsetX;
+            var y = event.offsetY;
+            var i = Math.floor(x / resolutions);
+            var j = Math.floor(y / resolutions);
+                    if (grid[i][j] == 1) {
+                        ctx.fillStyle = "white";
+                        ctx.fillRect(x, y, resolutions - 1, resolutions - 2);
+                        grid[i][j] == 0;
+                    }
+                    else {
+                        ctx.fillStyle = "green";
+                        ctx.fillRect(x, y, resolutions - 1, resolutions - 2);
+                        grid[i][j] == 1;
+                    }
+                
+        });
     };
 };
--- 7.ts
+++ 7.ts
@@ -10,19 +10,22 @@ function make2DArray(cols: number, rows: number) {
 
 
 let grid: any;
+let position: number;
 let cols: number;
 let rows: number;
 let resolutions: number = 10;
 var canvas: HTMLCanvasElement;
 var ctx: CanvasRenderingContext2D;
-
-
+let _canvasWidth: any =(<HTMLInputElement>document.getElementById('canvasWidth')).value;
+let _canvasHeight: any =(<HTMLInputElement>document.getElementById('canvasHeight')).value;
+var timer: any;
 
 function setup(){
 	canvas = <HTMLCanvasElement>document.getElementById('cnvs');
-    ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
-	canvas.width = 800;
-	canvas.height = 600;
+	ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+	position = 0;
+	canvas.width = _canvasWidth;
+	canvas.height =_canvasHeight;
 	cols = canvas.width / resolutions;
 	rows = canvas.height / resolutions; 
 	
@@ -48,12 +51,15 @@ function draw() {
                 ctx.fillRect(x, y, resolutions - 1, resolutions - 2);
             }
             else {
-                ctx.fillStyle = "white";
+				ctx.fillStyle = "white";
                 ctx.fillRect(x, y, resolutions - 1, resolutions - 2);
-            }
+			}
+			
         }
     }
 }
+
+
 
 
 function game() {
@@ -98,12 +104,77 @@ function countNeighbors(grid: any, x: number, y: number){
 	sum -= grid[x][y]; 
 	return sum;
 }
-
-window.onload = () => {
-    setup();
+for (var i=0; i<2; i++){
+document.getElementsByTagName('input')[i].onkeypress = 
+function NotANumber(e: any) {
 	
-	step.onclick = function() {
-		 setInterval("game()", 10);
+		  e = e || event;
+		  if (e.ctrlKey || e.altKey || e.metaKey) return;
+		  var chr = getChar(e);
+		  if (chr == null) return;
+		  if (chr < '0' || chr > '9') {
+			return false;
+		  }
+		}
+		function getChar(event: any) {
+		  if (event.which == null) {
+			if (event.keyCode < 32) return null;
+			return String.fromCharCode(event.keyCode) 
+		  }
+	
+		  if (event.which != 0 && event.charCode != 0) {
+			if (event.which < 32) return null;
+			return String.fromCharCode(event.which) 
+		  }
+	
+		  return null; 
+		}
+	}
+window.onload = () => {
+	
+	setup();
+	draw();
+	canvasWidth.onblur = function() {
+		
+		_canvasWidth = this.value;
+		clearInterval(timer);
+		setup();
+		draw();
+	  };
+
+	canvasHeight.onblur = function() {
+		_canvasHeight = this.value;
+		clearInterval(timer);
+		setup();
+		draw();
+	  };
+
+
+	start.onclick = function() {
+		
+		timer = setInterval("game()", 10);
+	  };
+	pause.onclick = function() {
+		
+		clearInterval(timer);
+		canvas.addEventListener('click', function (event) {
+			
+		var x = event.offsetX;
+		var y = event.offsetY;
+		var i = Math.floor(x / resolutions);
+		var j = Math.floor(y / resolutions);
+				if (grid[i][j] == 1) {
+					ctx.fillStyle = "white";
+					ctx.fillRect(x, y, resolutions - 1, resolutions - 2);
+					grid[i][j] == 0;
+				}
+				else {
+					ctx.fillStyle = "green";
+					ctx.fillRect(x, y, resolutions - 1, resolutions - 2);
+					grid[i][j] == 1;
+				}
+			
+	});
 	  };
 	
 }
